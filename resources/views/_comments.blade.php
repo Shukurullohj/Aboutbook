f2667ecf21fdd41385ee1b021e3a4d97086ecae0
= <=>
2021-01-30 22:22:21 +0900

    Comments section finished

---
+++ resources/views/_comments.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <form action="">
+        <div class="custom-file my-3">
+            <input type="file" class="custom-file-input" id="customFile">
+            <label class="custom-file-label" for="customFile">Choose Boook...</label>
+        </div>
+        <textarea rows="3" placeholder="Your comment about the book" class="w-100 form-control p-2"></textarea>
+        <div class="d-flex justify-content-between my-3 align-items-center">
+            <h5>@Username</h5>
+            <button type="submit" class="btn btn-primary ">Submit</button>
+        </div>
+    </form>
+
+</section>
---
+++ resources/views/_latest-upload-books.blade.php-tmp
@@ -0,0 +1,10 @@
+@foreach (range (1, 8) as $user)
+<div class="">
+    <h5 class="text-center text-secondary font-weight-bold">Latest Books</h5>
+    <img id="img-upload-book" class="d-block mx-auto" src="https://prodimage.images-bn.com/pimages/9780439358071_p0_v4_s1200x630.jpg" alt="">
+    <p class="text-center text-muted my-3">Author: Jerry</p>
+</div>
+@endforeach
+
+
+
